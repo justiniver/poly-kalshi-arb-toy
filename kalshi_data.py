1940836caf131d53e3bea96a04b2126c075aaae5
Justin Iverson <justiniver@icloud.com>
2025-11-21 22:36:29 -0500

    simple way to generate titles

--- kalshi_data.py
+++ kalshi_data.py
@@ -1,10 +1,31 @@
 import requests
 
-def get_all_contracts():
+def get_markets() -> list:
     series_url = "https://api.elections.kalshi.com/trade-api/v2/series"
     series = requests.get(series_url).json()["series"]
-# markets_url = f"https://api.elections.kalshi.com/trade-api/v2/series/{ticker}/markets"
-# markets = requests.get(markets_url).json()["markets"]
+    valid_markets = []
+    num_series = len(series)
+    for i in range(num_series - 10, num_series): # only looking at a few series for now
+        ticker = series[i]['ticker']
+        markets_url = f"https://api.elections.kalshi.com/trade-api/v2/markets?series_ticker={ticker}&status=open"
+        markets = requests.get(markets_url).json()["markets"]
+        for m in markets:
+            if m:
+                valid_markets.append(m)
 
+    return valid_markets
 
-print(series[1]['ticker'])
+def get_titles() -> list:
+    markets = get_markets()
+    titles = []
+    num_markets = len(markets)
+    for i in range(num_markets):
+        titles.append(markets[i]['title'])
+    
+    return titles
+
+markets = get_markets()
+titles = get_titles()
+print(titles[:10])
+
+print(len(titles))
