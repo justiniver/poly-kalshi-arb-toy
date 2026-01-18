e7f8ab1474cd6621cfb66f3cc877b16ddc2637b7
Justin Iverson <justiniver@icloud.com>
2026-01-18 14:31:41 -0500

    filtering method implemented (for now)

--- data_sources/kalshi_data_old.py
+++ data_sources/kalshi_data_old.py
@@ -8,7 +8,7 @@ def _generate_custom_desc(m: dict) -> str:
     builder = []
     title = m['title']
     subtitle = m['subtitle']
-    # category = m['category']
+    category = m['category']
     rules = m['rules_primary']
 
     # I think some of these are guaranteed to be nonempty but doing this anyway
@@ -16,52 +16,46 @@ def _generate_custom_desc(m: dict) -> str:
         builder.append(f"Market title: {title}")
     if subtitle:
         builder.append(f"Subtitle: {subtitle}.")
-    # if category:
-    #     builder.append(f"Category: {category}.")
+    if category:
+        builder.append(f"Category: {category}.")
     if rules:
         builder.append(f"Rules: {rules}")
 
     return " ".join(builder)
 
-def get_markets() -> list[dict]:
+# ill clean this up later but for now we default everything to None
+def get_markets(limit: int | None = None, filter: str | list[str] | None = None) -> list[dict]:
     series = requests.get(series_url).json()["series"]
-    print(series)
     valid_markets = []
     num_series = len(series)
+    if isinstance(filter, str):
+        filter = [filter]
+    count = 0
     for i in range(num_series):
+        category = series[i]['category'] # some fields live in series not markets
         ticker = series[i]['ticker']
         markets_url = f"https://api.elections.kalshi.com/trade-api/v2/markets?series_ticker={ticker}&status=open"
         markets = requests.get(markets_url).json()["markets"]
         for m in markets:
-            if m:
+            if m and (not filter or category in filter):
+                count += 1
                 m['url'] = f"https://kalshi.com/markets/{ticker}" # adding url entry in dict because it seems useful
+                m['category'] = category
                 m['custom_desc'] = _generate_custom_desc(m)
                 valid_markets.append(m)
+                if limit and count == limit:
+                    return valid_markets
 
     return valid_markets
 
-def get_one_market() -> dict:
-    series = requests.get(series_url).json()["series"]
-    print(series)
-    num_series = len(series)
-    for i in range(num_series):
-        ticker = series[i]['ticker']
-        markets_url = f"https://api.elections.kalshi.com/trade-api/v2/markets?series_ticker={ticker}&status=open"
-        markets = requests.get(markets_url).json()["markets"]
-        for m in markets:
-            if m:
-                m['url'] = f"https://kalshi.com/markets/{ticker}"
-                m['custom_desc'] = _generate_custom_desc(m)
-                return m
-
 def print_market_info() -> None:
-    m = get_one_market()
+    m = get_markets(1)[0]
     print('\n', m, '\n')
     print(m['title'])
     print(m['subtitle'])
     print(m['yes_sub_title'])
     print(m['no_sub_title'])
-    # print(m['category'])
+    print(m['category'])
     print(m['rules_primary'])
     print(m['custom_desc'])
 
